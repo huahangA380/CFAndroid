c984284094abdf31a3b0d3033c61aed31ccb0eb1
shizhiqiang <szq921618920@gmail.com>
2015-01-28 21:10:43 +0800

    add  sign_qq_up.xml

--- CroUniversity/src/com/crouniversity/main/MainActivity.java
+++ CroUniversity/src/com/crouniversity/main/MainActivity.java
@@ -71,6 +71,9 @@ public class MainActivity extends ActionBarActivity implements
 		case 3:
 			mTitle = getString(R.string.title_section3);
 			break;
+		case 4:
+			mTitle = getString(R.string.app_name);
+			break;
 		}
 	}
 
